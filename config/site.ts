403a8364bd108ee400c6f101a3f81d920e49b82a
Astra <abhaypratapsinghwd@gmail.com>
2025-08-23 09:17:17 +0530

    first commit

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "@/interfaces/site";
+
+export const siteConfig: SiteConfig = {
+  name: "MarketX",
+  description: "Automation Web Whatsapp Marketing",
+  url: "https://web-marketx.vercel.app",
+  keywords: ["Marketing", "Whatsapp Automation"],
+  links: {
+    twitter: "https://x.com/XSinghAstra",
+    github: "https://github.com/SinghAstra",
+    githubRepo: "https://github.com/SinghAstra/marketx",
+  },
+};
